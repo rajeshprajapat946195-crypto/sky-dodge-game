7cd539bd002e5c20721a32978f831d5943687b2b
rajeshprajapat946195-crypto <rajeshprajapat946195@gmail.com>
2026-04-21 09:46:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,31 @@
-window.onload = function () {
-  let startBtn = document.getElementById("startBtn");
-
-  let gameRunning = false;
-  let obstacleInterval;
-
-  startBtn.addEventListener("click", () => {
-    if (!gameRunning) {
-      gameRunning = true;
-      startBtn.style.display = "none";
-
-      obstacleInterval = setInterval(createObstacle, 1500);
-    }
-  });
-};
-let playerX = 130;
-
-document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowLeft" && playerX > 0) {
-    playerX -= 20;
-  }
-  if (e.key === "ArrowRight" && playerX < 260) {
-    playerX += 20;
-  }
-  player.style.left = playerX + "px";
-});
-
-function createObstacle() {
-  let obs = document.createElement("div");
-  obs.classList.add("obstacle");
-  obs.style.left = Math.floor(Math.random() * 260) + "px";
-  obs.style.top = "0px";
-  gameArea.appendChild(obs);
-
-  let fall = setInterval(() => {
-    let obsTop = parseInt(obs.style.top);
-    obs.style.top = obsTop + 5 + "px";
-
-    let playerRect = player.getBoundingClientRect();
-    let obsRect = obs.getBoundingClientRect();
-
-    if (
-      obsRect.bottom > playerRect.top &&
-      obsRect.top < playerRect.bottom &&
-      obsRect.left < playerRect.right &&
-      obsRect.right > playerRect.left
-    ) {
-     document.body.innerHTML = `
-  <h1>Game Over</h1>
-  <p>Your Score: ${score}</p>
-  <button onclick="location.reload()">Restart</button>
-`; 
-  
-    }
-
-    if (obsTop > 400) {
-      clearInterval(fall);
-      obs.remove();
-      setInterval(() => {
-  score++;
-  scoreDisplay.innerText = "Score: " + score;
-}, 1000);
-    document.addEventListener("touchmove", (e) => {
-  let touchX = e.touches[0].clientX;
-  player.style.left = (touchX - 20) + "px";
-});
-    
-  }, 50);
+body {
+    text-align: center;
+    font-family: Arial;
 }
 
-startBtn.addEventListener("click", () => {
-  if (!gameRunning) {
-    gameRunning = true;
-    startBtn.style.display = "none";
-   let gameRunning = false;
-let obstacleInterval;
+#gameArea {
+    width: 300px;
+    height: 400px;
+    background: lightblue;
+    margin: auto;
+    position: relative;
+    overflow: hidden;
+    border: 2px solid black;
+}
 
-startBtn.addEventListener("click", () => {
-  if (!gameRunning) {
-    gameRunning = true;
-    startBtn.style.display = "none";
+#player {
+    width: 40px;
+    height: 40px;
+    background: red;
+    position: absolute;
+    bottom: 10px;
+    left: 130px;
+}
 
-    obstacleInterval = setInterval(createObstacle, 1500);
-  }
-}); 
-  }
-});
+.obstacle {
+    width: 40px;
+    height: 40px;
+    background: black;
+    position: absolute;
+    top: 0;
+}
